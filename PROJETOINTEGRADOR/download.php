ec33a7fa5f2d8eff3b7e1b5775ef1ed47615d0d8
BrunoAlves016 <brunocioccia30@gmail.com>
2024-11-01 01:41:33 -0300

    PROJETIN INTEGRADOR
    ADD DOWNLOAD DO JOGO

---
+++ PROJETOINTEGRADOR/download.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilo.css">
+
+    <title>DOWNLOAD</title>
+</head>
+<body>
+<div class="container">
+        <div class="logo"><a href="inicio.php"><img src="img/perfil.png" width="100px" height="100px"></a></div>
+        <div class="social">
+            <a href="https://www.facebook.com" class="fb"><img src="icons/facebook.png" width="30px" height="30px"></a>
+            <a href="https://www.instagram.com" class="ins"><img src="icons/instagram.jpg" width="30px" height="30px"></a>
+        </div>
+    </div>
+    <div class="subtitulo">
+        <h1>FAÇA SEU DOWNLOAD AQUI</h1>
+    </div>
+    <div class="downloadjogo">
+    <a href="jogo/ProjectLostGuildOF.zip" download="">
+      <img src="img/bau.png" alt="W3Schools" width="150" height="140"></a>
+  </div>
+</body>
+</html>
+
+
+
